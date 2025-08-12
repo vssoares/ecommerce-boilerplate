778bb4bc107a61397f78ee084d60fc7d65da7215
Vinicius Soares <vsss23@hotmail.com>
2025-08-12 17:44:59 -0300

    core: adiciona configuração do ESLint e Prettier, implementa botão reutilizável e atualiza dependências

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div
+      className={cn(
+        "flex min-h-screen flex-col items-center justify-center p-4",
+      )}
+    >
+      <h1 className={cn("mb-4 text-4xl font-bold")}>
+        Welcome to the Home Page
+      </h1>
+      <Button className="bg-red-600 px-5">Teste</Button>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
